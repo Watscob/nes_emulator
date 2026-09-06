7860a80ff48adeae60a4d9806d56e2002c5c61d9
Etienne Chanudet <chanudet.etienne@gmail.com>
2026-09-06 11:43:16 +0200

    Accept only mapper 0

--- lib/src/nes_cartridge.cpp
+++ lib/src/nes_cartridge.cpp
@@ -43,6 +43,8 @@ void NesCartridge::init_(const std::vector<uint8_t>& raw)
         throw std::runtime_error("Invalid NES tag.");
 
     mapper_ = (raw.at(7u) & 0xF0) | (raw.at(6u) >> 4u);
+    if (mapper_ != 0u)
+        throw std::runtime_error("Only mapper 0 is supported.");
 
     uint8_t ines_version = (raw.at(7u) >> 2u) & 0x3;
     if (ines_version != 0u)
